22882677b404683f26f894ad8ff2793ffae9487e
Tal Roelof Van Daalen <tal.van.daalen@cern.ch>
2020-05-12 18:00:43 +0200

    Adding updated singletop mod and weight config files with consistent conventions + SystRegionHistoScript.py changed accordingly

--- src/VLQAnalysis/python/SystRegionHistoScript.py
+++ src/VLQAnalysis/python/SystRegionHistoScript.py
@@ -4,8 +4,8 @@ import sys
 import importlib
 
 # default params
-indir  = '/nfs/pic.es/user/t/tvdaalen/scratch2/SingleVLQWorkArea/RootFiles/RootFiles_Singletop_LooseSystRegs/'
-indir_alt = '/nfs/pic.es/user/t/tvdaalen/scratch2/SingleVLQWorkArea/RootFiles/RootFiles_Singletop_LooseSystRegs/AltSamples/'
+indir  = '/nfs/pic.es/user/t/tvdaalen/scratch2/SingleVLQWorkArea/RootFiles/RootFiles_Singletop_UncTest_FitInputs/'
+indir_alt = indir+'SingletopAlt/'
 var = 'meff'
 regDict = "regions_dictionary_sVLQ"
 
@@ -15,7 +15,7 @@ startFromBackup = True
 doPMGweights = False
 doModSysts = True
 
-samples = ['SingletopWt','Singletopschan','Singletoptchan'] #'Singletop',
+samples = ['Singletop'] #SingletopWtprod','Singletopschan','Singletoptchan']
 
 # user input
 if(len(sys.argv))>1:
@@ -105,29 +105,57 @@ for campaign in ['a','d','e']:
             for fr in fitRegions:
                 # doing pmg weight histogram replacing
                 # we are replacing h_sys_SR by h_nom_SR * h_sys_loose / h_nom_loose
+
+                # set uncertainties to 0 if histogram has no events
+                emptyhist = False
+                try:
+                    h_nom_SR = outf.Get(fr+'_'+var)
+                    if h_nom_SR.Integral() == 0.:
+                        print "\nRegion %s is empty in sample %s. Setting uncertainties to 0.0!"%(fr,fname_short)
+                        emptyhist = True
+
+                except ReferenceError:
+                    print "Could not find histogram(s) for %s. Skipping..."%(fr)
+                    continue
+                                
                 for suf in histoSuffixes:
                     try:
                         h_nom_SR = outf.Get(fr+'_'+var)
-                        h_nom_loose = outf.Get(regMap[fr]+'_'+var)
-                        h_sys_loose = outf.Get(regMap[fr]+'_'+var+suf)
-                        h_sys_loose.Divide(h_nom_loose)
-                        h_nom_SR.Multiply(h_sys_loose)
-                        h_nom_SR.Write(fr+'_'+var+suf)
+
+                        if emptyhist:
+                            h_nom_SR.Write(fr+'_'+var+suf)
+                            continue
+
+                        else:
+                            h_nom_loose = outf.Get(regMap[fr]+'_'+var)
+                            h_sys_loose = outf.Get(regMap[fr]+'_'+var+suf)
+                            h_sys_loose.Divide(h_nom_loose)
+                            h_nom_SR.Multiply(h_sys_loose)
+                            h_nom_SR.Write(fr+'_'+var+suf)
 
                     except ReferenceError:
                         print "Could not find histogram(s) for %s. Skipping..."%(fr)
+                        continue
 
         if doModSysts:
+
+            fname_short_AFII = sample+campaign+'.root'
+            fname_AFII = indir_alt+fname_short
 
             # doing modelling systematic histogram replacing
             # we are replacing h_alt_SR by h_nom_SR * h_alt_loose / h_nom_loose
             for mod in modSysts:
 
                 # only do DiagSub for Wt channel
-                if mod == 'DiagSub' and 'Wt' not in sample:
-                    continue
+                if mod == 'DiagSub':
+                    # if 'Wt' not in sample:
+                    #     continue
+                    # else:
+                    # use fullsim sample as nominal for DS syst
+                    fname_short_AFII = sample+'.mc16'+campaign+'.root'
+                    fname_AFII = indir+fname_short_AFII
 
-                fname_alt_short = sample+mod+'.mc16'+campaign+'.root'
+                fname_alt_short = sample+'_'+mod+'.mc16'+campaign+'.root'
                 fname_alt = indir_alt+fname_alt_short
 
                 # check if file exists
@@ -151,15 +179,33 @@ for campaign in ['a','d','e']:
                     nameSR = fr+'_'+var
                     nameLoose = regMap[fr]+'_'+var
 
+                    outf = TFile(fname_AFII, 'read')
+
+                    # set uncertainties to 0 if histogram has no events
+                    emptyhist = False
                     try:
-                        outf = TFile(fname, 'read')
+                        h_nom_SR = outf.Get(nameSR)
+                        if h_nom_SR.Integral() == 0.:
+                            print "\nRegion %s is empty in sample %s. Setting uncertainties to 0.0!"%(fr,fname_short_AFII)
+                            emptyhist = True
+
+                    except ReferenceError:
+                        print "Could not find histogram(s) for %s. Skipping..."%(fr)
+                        continue
+                                    
+                    try:
                         outf.cd()
 
-                        h_nom_SR = outf.Get(nameSR)
+                        h_nom_SR = outf.Get(nameSR).Clone()
+                        h_nom_SR_OriginalIntegral = h_nom_SR.Integral()
                         h_nom_loose = outf.Get(nameLoose)
 
                         outf_alt = TFile(fname_alt, 'update')
                         outf_alt.cd()
+
+                        if emptyhist:
+                            h_nom_SR.Write(nameSR)
+                            continue
 
                         h_alt_loose = outf_alt.Get(nameLoose)
                         h_alt_SR = outf_alt.Get(nameSR)
@@ -167,9 +213,16 @@ for campaign in ['a','d','e']:
                         h_alt_loose.Divide(h_nom_loose)
                         h_nom_SR.Multiply(h_alt_loose)
 
-                        print '\n',sample, mod, fr
+                        if h_nom_SR.Integral() == 0. or h_nom_SR.Integral() < 0.:
+                            print '\nNew histogram integral <= 0 - Using nominal histogram as alt. instead...'
+                            outf.cd()
+                            h_nom_SR = outf.Get(nameSR).Clone()
+                            outf_alt.cd()
+
+                        print '\n',sample, 'mc16'+campaign, mod, fr
                         print 'h_alt_SR:\t', h_alt_SR.Integral()
                         print 'h_alt_new:\t', h_nom_SR.Integral()
+                        print 'h_nom_SR:\t', h_nom_SR_OriginalIntegral
 
                         h_nom_SR.Write(nameSR)
 
@@ -178,6 +231,7 @@ for campaign in ['a','d','e']:
 
                     except ReferenceError:
                         print "Could not find histogram(s) for %s. Skipping..."%(fr)
+                        continue
 
                 outf_alt.Close()
 
