88ee286d8e72b96a4268c8ea4582bc659379d842
Tal Roelof Van Daalen <tal.van.daalen@cern.ch>
2020-06-05 17:31:02 +0200

    Adding macros_tal with some useful macros

---
+++ src/VLQAnalysis/macros/macros_tal/GetTtbarFlavFractions.py
@@ -0,0 +1,32 @@
+from ROOT import *
+
+# pathbase = '/nfs/atlas-data07/tvdaalen/SingleVLQWorkArea/RootFiles/RootFiles_Ttbar+Singletop_Syst/'
+# pathbase = '/nfs/atlas-data07/tvdaalen/SingleVLQWorkArea/RootFiles/RootFiles_Ttbar_HF_reweighting/HFunreweighted/'
+pathbase = '/nfs/atlas-data07/tvdaalen/SingleVLQWorkArea/RootFiles/RootFiles_Ttbar_HF_reweighting/HFreweighted/'
+campaigns = ['a']#,'d','e']
+flavs = ['ttbarlight','ttbarcc','ttbarbb']
+gens = ['AFII','PowHer','aMCPy'] #['Nominal_AFII','PowhegHerwig7','aMcAtNloPythia8']
+
+nominal_gen = 'AFII'
+
+evnts = {}
+
+for gen in gens:
+    evnts[gen] = {}
+    evnts[gen]['total'] = 0.
+    for flav in flavs:
+        evnts[gen][flav] = 0.
+        for campaign in campaigns:
+            fn = pathbase+flav+'_'+gen+'.mc16'+campaign+'.root'
+            f = TFile(fn,'read')
+            h = f.Get("c1lep3jin1bin_meff")
+            evnts[gen][flav] += h.Integral()
+            evnts[gen]['total'] += h.Integral()
+
+for gen in gens:
+    print gen
+    for flav in flavs:
+        print flav
+        # print 'ratio to nominal:',evnts[gen][flav]/evnts['Nominal_AFII'][flav],'\tfraction of total:',evnts[gen][flav]/evnts[gen]['total'],'\tratio to nominal of fraction:',(evnts[gen][flav]/evnts[gen]['total'])/(evnts['Nominal_AFII'][flav]/evnts['Nominal_AFII']['total'])
+        print 'ratio to nominal:',evnts[gen][flav]/evnts[nominal_gen][flav],'\tfraction of total:',evnts[gen][flav]/evnts[gen]['total'],'\tratio to nominal of fraction:',(evnts[gen][flav]/evnts[gen]['total'])/(evnts[nominal_gen][flav]/evnts[nominal_gen]['total'])
+    print ""
