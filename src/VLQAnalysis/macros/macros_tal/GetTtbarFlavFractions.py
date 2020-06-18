e4f4c1769285628532c09235921f0239d6265a20
Tal Roelof Van Daalen <tal.van.daalen@cern.ch>
2020-06-18 12:07:06 +0200

    Small change to regions_dictionary_sVLQ, correcting included presel regions and corresponding plot labels

--- src/VLQAnalysis/macros/macros_tal/GetTtbarFlavFractions.py
+++ src/VLQAnalysis/macros/macros_tal/GetTtbarFlavFractions.py
@@ -2,8 +2,9 @@ from ROOT import *
 
 # pathbase = '/nfs/atlas-data07/tvdaalen/SingleVLQWorkArea/RootFiles/RootFiles_Ttbar+Singletop_Syst/'
 # pathbase = '/nfs/atlas-data07/tvdaalen/SingleVLQWorkArea/RootFiles/RootFiles_Ttbar_HF_reweighting/HFunreweighted/'
-pathbase = '/nfs/atlas-data07/tvdaalen/SingleVLQWorkArea/RootFiles/RootFiles_Ttbar_HF_reweighting/HFreweighted/'
-campaigns = ['a']#,'d','e']
+# pathbase = '/nfs/atlas-data07/tvdaalen/SingleVLQWorkArea/RootFiles/RootFiles_Ttbar_HF_reweighting/HFreweighted/'
+pathbase = '/nfs/atlas-data07/tvdaalen/VLQAnalysisRun2/FitInputs_RW_Jun2020/FilesTRexF/TtStAlt/'
+campaigns = ['a','d','e']
 flavs = ['ttbarlight','ttbarcc','ttbarbb']
 gens = ['AFII','PowHer','aMCPy'] #['Nominal_AFII','PowhegHerwig7','aMcAtNloPythia8']
 
@@ -17,7 +18,12 @@ for gen in gens:
     for flav in flavs:
         evnts[gen][flav] = 0.
         for campaign in campaigns:
-            fn = pathbase+flav+'_'+gen+'.mc16'+campaign+'.root'
+
+            if gen == nominal_gen:
+                fn = pathbase+flav+'.mc16'+campaign+'.root'
+            else:
+                fn = pathbase+flav+gen+'.mc16'+campaign+'.root'
+
             f = TFile(fn,'read')
             h = f.Get("c1lep3jin1bin_meff")
             evnts[gen][flav] += h.Integral()
@@ -28,5 +34,5 @@ for gen in gens:
     for flav in flavs:
         print flav
         # print 'ratio to nominal:',evnts[gen][flav]/evnts['Nominal_AFII'][flav],'\tfraction of total:',evnts[gen][flav]/evnts[gen]['total'],'\tratio to nominal of fraction:',(evnts[gen][flav]/evnts[gen]['total'])/(evnts['Nominal_AFII'][flav]/evnts['Nominal_AFII']['total'])
-        print 'ratio to nominal:',evnts[gen][flav]/evnts[nominal_gen][flav],'\tfraction of total:',evnts[gen][flav]/evnts[gen]['total'],'\tratio to nominal of fraction:',(evnts[gen][flav]/evnts[gen]['total'])/(evnts[nominal_gen][flav]/evnts[nominal_gen]['total'])
+        print 'ratio to nominal:',evnts[gen][flav]/evnts[nominal_gen][flav],'\tfraction of total:',evnts[gen][flav]/evnts[gen]['total'],'\tratio to nominal of fraction:',(evnts[nominal_gen][flav]/evnts[nominal_gen]['total'])/(evnts[gen][flav]/evnts[gen]['total'])
     print ""
