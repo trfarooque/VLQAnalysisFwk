7a9c7ed435a2edba191e915c5054626035b0b9e0
carlos.josue.buxo.vazquez@cern.ch <cbuxovaz@at303.pic.es>
2021-05-17 19:46:59 +0200

    Adding region dictionary containing optimized binning for the 1L SRs.

---
+++ src/VLQAnalysis/python/regions_dictionary_pVLQ_newAna_merged_regions.py
@@ -0,0 +1,191 @@
+# Created with MakeRegionsDictionary at 2021-04-11 19:46:47.845114
+
+#
+#
+# Fit regions
+#
+#
+reg_1lep6jin3bex0Hex0Vin0LTex = {
+	'name':"HTX_c1lep6jin3bex0Hex0Vin0LTex",
+        'legend': "#scale[0.75]{#geq6j, 3b, 0(t_{h}+t_{l}), 0h, 0V}",
+        'binning':"0,700,850,1300,2300,2800,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+        #'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin3bex0Hex0Vin1LTin = {
+	'name':"HTX_c1lep6jin3bex0Hex0Vin1LTin",
+	'legend': "#scale[0.75]{#geq6j, 3b, 1(t_{h}+t_{l}), 0h, 0V}",
+        'binning':"0,1000,1250,1850,2700,3350,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+	#'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin3bex1Hex0Vex0LTex = {
+	'name':"HTX_c1lep6jin3bex1Hex0Vex0LTex",
+	'legend': "#scale[0.75]{#geq6j, 3b, 0(t_{h}+t_{l}), 1h, 0V}",
+        'binning':"1000,1200,1750,2500,3150,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+	#'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin3bex1Hex0Vex1Lin0Tex = {
+	'name':"HTX_c1lep6jin3bex1Hex0Vex1Lin0Tex",
+	'legend': "#scale[0.75]{#geq6j, 3b, 0t_{h}, 1t_{l}, 1h, 0V}",
+        'binning':"1000,1350,1600,2000,2550,7000",
+        #'binning':"1000,1150,1350,1600,2000,2550,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+	#'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin3bex1Hex0Vin0Lex1Tin = {
+	'name':"HTX_c1lep6jin3bex1Hex0Vin0Lex1Tin",
+	'legend': "#scale[0.75]{#geq6j, 3b, 1t_{h}, 0t_{l}, 1h, 0V}",
+        'binning':"1000,1300,1550,1950,2350,2800,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+        #'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin3bex1Hex0Vin1Lin1Tin = {
+	'name':"HTX_c1lep6jin3bex1Hex0Vin1Lin1Tin",
+	'legend': "#scale[0.75]{#geq6j, 3b, 1t_{h}, 1t_{l}, 1h, 0V}",
+        'binning':"1000,1650,1950,2250,2550,2900,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+	#'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin3bex2Hin0Vin0LTex = {
+	'name':"HTX_c1lep6jin3bex2Hin0Vin0LTex",
+	'legend': "#scale[0.75]{#geq6j, 3b, 0(t_{h}+t_{l}), 2h, 0V}",
+        'binning':"1000,1350,1650,2300,2850,3400,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+	#'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin4bin0Hex0Vin0LTex = {
+	'name':"HTX_c1lep6jin4bin0Hex0Vin0LTex",
+	'legend': "#scale[0.75]{#geq6j, 4b, 0(t_{h}+t_{l}), 0h, 0V}",
+        'binning':"0,700,850,1150,2000,2550,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+        #'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin4bin0Hex0Vin1LTin = {
+	'name':"HTX_c1lep6jin4bin0Hex0Vin1LTin",
+	'legend': "#scale[0.75]{#geq6j, 4b, 1(t_{h}+t_{l}), 0h, 0V}",
+        'binning':"0,1050,1300,1700,2100,2500,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+	#'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin4bin1Hex0Vex0LTex = {
+	'name':"HTX_c1lep6jin4bin1Hex0Vex0LTex",
+	'legend': "#scale[0.75]{#geq6j, 4b, 0(t_{h}+t_{l}), 1h, 0V}",
+        'binning':"1000,1250,1600,2050,2450,7000",
+        #'binning':"1000,1050,1250,1600,2050,2450,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+	#'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin4bin1Hex0Vex1Lin0Tex = {
+	'name':"HTX_c1lep6jin4bin1Hex0Vex1Lin0Tex",
+	'legend': "#scale[0.75]{#geq6j, 4b, 0t_{h}, 1t_{l}, 1h, 0V}",
+        'binning':"1000,1400,1650,1900,2250,7000",
+        #'binning':"1000,1150,1400,1650,1900,2250,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+	#'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin4bin1Hex0Vin0Lex1Tin = {
+	'name':"HTX_c1lep6jin4bin1Hex0Vin0Lex1Tin",
+	'legend': "#scale[0.75]{#geq6j, 4b, 1t_{h}, 0t_{l}, 1h, 0V}",
+        'binning':"1000,1400,1700,2050,2400,2800,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+        #'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin4bin1Hex0Vin1Lin1Tin = {
+	'name':"HTX_c1lep6jin4bin1Hex0Vin1Lin1Tin",
+	'legend': "#scale[0.75]{#geq6j, 4b, 1t_{h}, 1t_{l}, 1h, 0V}",
+        'binning':"1000,1700,2000,2350,2750,3200,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+        #'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin3bex1Hex1Vin0Lin0Tex = {
+	'name':"HTX_c1lep6jin3bex1Hex1Vin0Lin0Tex",
+	'legend': "#scale[0.75]{#geq6j, 3b, 0t_{h}, 0t_{l}, 1h, 1V}",
+        'binning':"1000,1400,1700,2050,2350,2750,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+	#'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin4bin1Hex1Vin0Lin0Tex = {
+	'name':"HTX_c1lep6jin4bin1Hex1Vin0Lin0Tex",
+	'legend': "#scale[0.75]{#geq6j, 4b, 0t_{h}, 0t_{l}, 1h, 1V}",
+        'binning':"1000,1550,1900,2250,2600,3050,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+        #'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin3bex2Hin0Vin1LTin = {
+	'name':"HTX_c1lep6jin3bex2Hin0Vin1LTin",
+	'legend': "#scale[0.75]{#geq6j, 3b, 1(t_{h}+t_{l}), 2h, 0V}",
+        'binning':"1000,1800,2200,2750,3300,3750,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+	#'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+reg_1lep6jin4bin2Hin0Vin0Lin0Tin = {
+	'name':"HTX_c1lep6jin4bin2Hin0Vin0Lin0Tin",
+	'legend': "#scale[0.75]{#geq6j, 4b, 0t_{h}, 0t_{l}, 2h, 0V}",
+        'binning':"1000,1600,2000,2500,2950,3400,7000",
+        #'binning':"1000,1100,1300,1700,2100,2500,5000",
+        #'binning':"\"AutoBin\",\"TransfoD\",3,3",
+	'type':"SIGNAL"
+}
+#
+#
+# Preselection regions
+#
+#
+reg_1lep6jin3bex = {
+	'name':"HTX_c1lep6jin3bex",
+	'legend': "#scale[0.75]{#geq6j, 3b}",
+	'binning':"700,800,1000,1400,1600,1800,3000",
+	'type':"VALIDATION"
+}
+reg_1lep6jin4bin = {
+	'name':"HTX_c1lep6jin4bin",
+	'legend': "#scale[0.75]{#geq6j, 4b}",
+	'binning':"700,800,1000,1400,1600,1800,3000",
+	'type':"VALIDATION"
+}
+
+fit_regions_1l = [
+reg_1lep6jin3bex0Hex0Vin0LTex,
+reg_1lep6jin3bex0Hex0Vin1LTin,
+reg_1lep6jin3bex1Hex0Vex0LTex,
+reg_1lep6jin3bex1Hex0Vex1Lin0Tex,
+reg_1lep6jin3bex1Hex0Vin0Lex1Tin,
+reg_1lep6jin3bex1Hex0Vin1Lin1Tin,
+reg_1lep6jin3bex2Hin0Vin0LTex,
+reg_1lep6jin4bin0Hex0Vin0LTex,
+reg_1lep6jin4bin0Hex0Vin1LTin,
+reg_1lep6jin4bin1Hex0Vex0LTex,
+reg_1lep6jin4bin1Hex0Vex1Lin0Tex,
+reg_1lep6jin4bin1Hex0Vin0Lex1Tin,
+reg_1lep6jin4bin1Hex0Vin1Lin1Tin,
+reg_1lep6jin3bex1Hex1Vin0Lin0Tex,
+reg_1lep6jin4bin1Hex1Vin0Lin0Tex,
+reg_1lep6jin3bex2Hin0Vin1LTin,
+reg_1lep6jin4bin2Hin0Vin0Lin0Tin
+]
+
+preselection_regions_1l = [
+reg_1lep6jin3bex,
+reg_1lep6jin4bin
+]
+all_regions_1l =  []
+all_regions_1l += fit_regions_1l
+all_regions_1l += preselection_regions_1l
